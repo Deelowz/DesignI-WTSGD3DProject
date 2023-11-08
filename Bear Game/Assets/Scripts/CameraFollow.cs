5b4008c6795c8de4bddc6ea67f2a6bbc49c9a6b8
Deelowz <Deelow96cn@mail.com>
2023-11-07 20:07:30 -0500

    Chad cam changes
    Camera perspective lowered and raised forward somewhat.

--- Bear Game/Assets/Scripts/CameraFollow.cs
+++ Bear Game/Assets/Scripts/CameraFollow.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target; 
-    public Vector3 offset;  
+    public Transform player;
+    public LayerMask mask;
+
+    private Vector3 offset; 
+
+    void Start()
+    {
+        offset = transform.position - player.position;
+    }
 
     void LateUpdate()
     {
-        if (target == null)
-            return;
+        Vector3 targetPosition = player.position + offset;
 
-        transform.position = target.position + offset;
+        RaycastHit hit;
+        if (Physics.Linecast(player.position, targetPosition, out hit, mask))
+        {
+            transform.position = hit.point + hit.normal * 0.2f;
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, 0.1f);
+        }
     }
 }
