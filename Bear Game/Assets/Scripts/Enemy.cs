bed93cb8e5844bd75a0725e3a930cf2c70e21049
Deelowz <Deelow96cn@mail.com>
2023-11-14 19:43:29 -0500

    Enemy update
    Enemy attacks and tracks the player

--- Bear Game/Assets/Scripts/Enemy.cs
+++ Bear Game/Assets/Scripts/Enemy.cs
@@ -1,38 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
+ 
 public class Enemy: MonoBehaviour
 {
     [SerializeField] float health = 3;
-
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] GameObject ragdoll;
+ 
+    [Header("Combat")]
+    [SerializeField] float attackCD = 3f;
+    [SerializeField] float attackRange = 1f;
+    [SerializeField] float aggroRange = 4f;
+ 
     GameObject player;
+    NavMeshAgent agent;
     Animator animator;
-    // Start is called before the first frame update
+    float timePassed;
+    float newDestinationCD = 0.5f;
+ 
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent == null || player == null)
+    {
+        // Log an error or return early if agent or player is not set.
+        Debug.LogError("Agent or player is null.");
+        return;
+    }
+        animator.SetFloat("speed", agent.velocity.magnitude / agent.speed);
+ 
+        if (player == null)
+        {
+            return;
+        }
+ 
+        if (timePassed >= attackCD)
+        {
+            if (Vector3.Distance(player.transform.position, transform.position) <= attackRange)
+            {
+                animator.SetTrigger("attack");
+                timePassed = 0;
+            }
+        }
+        timePassed += Time.deltaTime;
+ 
+        if (newDestinationCD <= 0 && Vector3.Distance(player.transform.position, transform.position) <= aggroRange)
+        {
+            newDestinationCD = 0.5f;
+            agent.SetDestination(player.transform.position);
+        }
+        newDestinationCD -= Time.deltaTime;
+        transform.LookAt(player.transform);
 
+        
+    }
+ 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            print(true);
+            player = collision.gameObject;
+        }
+    }
+ 
+    void Die()
+    {
+        animator.SetTrigger("death");
+        StartCoroutine(DestroyAfterDelay(2.0f));
+        Destroy(this.gameObject);
+    }
+ 
     public void TakeDamage(float damageAmount)
     {
         health -= damageAmount;
         animator.SetTrigger("damage");
-
+ 
         if (health <= 0)
         {
             Die();
         }
     }
-
-    void Die()
+    public void StartDealDamage()
     {
-        Destroy(this.gameObject);
+        GetComponentInChildren<EnemyCombat>().StartDealDamage();
     }
-    // Update is called once per frame
-    void Update()
+    public void EndDealDamage()
     {
-        
+        GetComponentInChildren<EnemyCombat>().EndDealDamage();
+    }
+ 
+    public void HitVFX(Vector3 hitPosition)
+    {
+        GameObject hit = Instantiate(hitVFX, hitPosition, Quaternion.identity);
+        Destroy(hit, 3f);
+    }
+ 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, aggroRange);
+    }
+    IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Destroy the GameObject after the specified delay.
+        Destroy(gameObject);
     }
 }
